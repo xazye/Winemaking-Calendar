22342ef839c724bb743e467c9ec4f362d568e1be
xazye <x42y13@gmail.com>
2022-10-02 21:39:38 +0200

    refactor to pure css

--- src/App.tsx
+++ src/App.tsx
@@ -1,125 +1,23 @@
-import { createStyles, Container, Title, Text, Button, BackgroundImage } from '@mantine/core';
+import { hover } from "@testing-library/user-event/dist/hover";
+import CSS from "csstype";
 import background from "./media/background.jpg";
-
-const useStyles = createStyles((theme) => ({
-  root: {
-    backgroundColor: '#11284b',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height:'100vh',
-    boxSizing:'border-box',
-    backgroundImage:
-      `linear-gradient(250deg, rgba(130, 201, 30, 0) 0%, #062343 70%),url(${background})`,
-    paddingTop: theme.spacing.xl * 3,
-    paddingBottom: theme.spacing.xl * 3,
-  },
-
-  inner: {
-    display: 'flex',
-    justifyContent: 'space-between',
-
-    [theme.fn.smallerThan('md')]: {
-      flexDirection: 'column',
-    },
-  },
-
-  image: {
-    [theme.fn.smallerThan('md')]: {
-      display: 'none',
-    },
-  },
-
-  content: {
-    paddingTop: theme.spacing.xl * 2,
-    paddingBottom: theme.spacing.xl * 2,
-    marginRight: theme.spacing.xl * 3,
-
-    [theme.fn.smallerThan('md')]: {
-      marginRight: 0,
-    },
-  },
-
-  title: {
-    color: theme.white,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontWeight: 900,
-    lineHeight: 1.05,
-    maxWidth: 500,
-    fontSize: 48,
-
-    [theme.fn.smallerThan('md')]: {
-      maxWidth: '100%',
-      fontSize: 34,
-      lineHeight: 1.15,
-    },
-  },
-
-  description: {
-    color: theme.white,
-    opacity: 0.75,
-    maxWidth: 500,
-
-    [theme.fn.smallerThan('md')]: {
-      maxWidth: '100%',
-    },
-  },
-
-  control: {
-    paddingLeft: 50,
-    paddingRight: 50,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontSize: 22,
-
-    [theme.fn.smallerThan('md')]: {
-      width: '100%',
-    },
-  },
-}));
-
-export function HeroImageRight() {
-  const { classes } = useStyles();
+import "./App.css";
+const HeroLanding = () => {
   return (
-    <div className={classes.root}>
-      <Container size="lg">
-        <div className={classes.inner}>
-          <div className={classes.content}>
-            <Title className={classes.title}>
-              Twój{' '}
-              <Text
-                component="span"
-                inherit
-                variant="gradient"
-                gradient={{ from: 'pink', to: 'yellow' }}
-              >
-               Kalendarzyk Winiarza
-              </Text>{' '}
-             jest gotowy Ci pomóc.
-            </Title>
-
-            <Text className={classes.description} mt={30}>
-            Przekonaj się jak łatwe potrafi być tworzenie i przechowywanie notatek o twoich trunkach! Zapisuj swoje ukochane przepisy w chmurze, przeglądaj zdjęcia swoich seksownych nastawów, a wszystko to
-            <Text
-                component="span"
-                inherit
-                variant="gradient"
-                gradient={{ from: 'pink', to: 'blue' }}
-              > ZA DARMO.
-              </Text>
-            </Text>
-
-            <Button
-              variant="gradient"
-              gradient={{ from: 'pink', to: 'yellow' }}
-              size="xl"
-              className={classes.control}
-              mt={40}
-            >
-              Ayyyyyyyy, wpuść mnie bydlaku
-            </Button>
-          </div>
+    <div className="root">
+      <div className="inner">
+        <h1 className="title">
+          Twój <span className="hlght">Kalendarzyk Winiarza</span> jest gotowy
+          Ci pomóc.
+        </h1>
+        <div className="description">
+          Przekonaj się jak łatwe potrafi być tworzenie i przechowywanie notatek
+          o twoich trunkach! Zapisuj swoje ukochane przepisy w chmurze,
+          przeglądaj zdjęcia swoich seksownych nastawów, a wszystko to ZA DARMO.
         </div>
-      </Container>
+        <div className="control">Ayyyyyyy, wpuść mnie</div>
+      </div>
     </div>
   );
-}
-export default HeroImageRight
+};
+export default HeroLanding;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { HeroImageRight } from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
+import HeroLanding from "./App";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HeroImageRight />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HeroLanding />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
